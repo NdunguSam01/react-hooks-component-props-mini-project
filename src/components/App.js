d3f41711dd02637f811bff5fe7721050ea5f8d35
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-09 10:28:14 +0300

    All tests passed

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,11 @@ import ArticleList from "./ArticleList";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
       <ArticleList posts={blogData.posts}/>
-    </>
+    </div>
   );
 }
 
